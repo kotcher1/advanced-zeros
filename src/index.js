9396215eebf78b00a5f68f416b40c88fdac89987
kotcher1 <annashkt@gmail.com>
2018-10-05 00:34:57 +0300

    zeroes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+function calcPrime(num) {
+  let prime = null;
+  let pow = 0;
+  let primesNum = [];
+  let cn = 2;
+  do {
+    if (num % cn === 0) {
+      primesNum.push(cn);
+      num = num / cn;
+    } else {
+      cn++;
+    };
+  }
+  while (cn <= num);
+  prime = Math.max(...primesNum);
+  pow = primesNum.filter(function(el) {return el === prime}).length;
+  return [prime, pow];
+}
+
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  const [prime, pow] = calcPrime(base);
+  let s = 1;
+  let count = Math.pow(prime, s);
+  let zeroes = 0;
+  for (; number >= count; count = Math.pow(prime, ++s)) {
+    zeroes = zeroes + Math.trunc(number / count);
+  }
+  return Math.floor(zeroes / pow);
 }
